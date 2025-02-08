e85ceb26cfe8647a014f8273b70f924a7c67c1ad
yuvikakathaith <yuvikakathaith3103@gmail.com>
2025-02-08 18:18:00 +0530

    commit 3

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 import { User } from "../models/userSchema.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export const register = async(req, res, next) => {
     const {name, email, phone, password} = req.body;
@@ -38,25 +39,38 @@ export const login = async(req, res, next) => {
         return next(
             res.status(400).json({
                 success: false,
-                message: "Invalid email or password."
+                message: "Please fill full form."
             })
         )
     }
     const user = await User.findOne({email});
     if(!user){
-        
+        return next(
+            res.status(400).json({
+                success: false,
+                message: "Invalid email or password!"
+            })
+        )
     }
-    const isPasswordMatching = bcrypt.compare(password, user.password);
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
     if(!isPasswordMatching){
         return next(
             res.status(400).json({
                 success: false,
-                message: "Invalid email or password."
+                message: "Incorrect Password!"
             })
         )
     }
-    res.status(200).json({
+    const token = await jwt.sign({id: user._id}, process.env.JWT_secret, {
+        expiresIn: process.env.tokenExpiresIn
+    })
+    res.status(200).cookie("token", token , {
+            httpOnly: true,
+            expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000)
+        }
+    ).json({
         success: true,
-        message: "User logged in!"
+        message: "User logged in!",
+        user, token
     })
 }
