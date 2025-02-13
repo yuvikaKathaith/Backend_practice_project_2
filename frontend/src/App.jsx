9aec258e53ba0748256c60ea3f3ccd14ffb0aa01
yuvikakathaith <yuvikakathaith3103@gmail.com>
2025-02-13 16:22:29 +0530

    commit 9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-
+import Navbar from './components/Navbar.jsx'
+import Home from './components/Home.jsx'
+import Register from './components/Register.jsx'
+import Login from './components/Login.jsx'
 
 const App = () => {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path='/'/>
-          <Route path='/register'/>
-          <Route path='/login'/>
+          <Route path='/' element={Home} />
+          <Route path='/register' element={Register}/>
+          <Route path='/login' element={Login}/>
         </Routes>
       </Router>
     </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
+import { createContext, StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+export const Context = createContext({
+  isAuthenticated: false,
+})
+
+export const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
+
+  return(
+    <Context.Provider 
+      value={ {isAuthenticated, setIsAuthenticated, user, setUser} }
+    >
+      <App />
+    </Context.Provider>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppWrapper />
   </StrictMode>,
 )
